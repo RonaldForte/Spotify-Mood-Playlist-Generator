cafab65471f545bf19185cfa1249c33f28cf6030
RonaldForte <ronaldjrforte@gmail.com>
2024-07-28 14:39:52 -0400

    File Changes

--- Generatify/src/App.js
+++ Generatify/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+//const CLIENT_ID = %PUT_ID_HERE%
+//const SECRET_ID = %PUT_ID_HERE%
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+  return(
+    <div class="container">
+      <h1 class="heading">Generate a Spotify Playlist</h1>
+      <input id="moodInput" class="input" type="text" placeholder="Enter your mood"></input>
+      <button id="searchButton" class="search">Search</button>
     </div>
   );
 }
